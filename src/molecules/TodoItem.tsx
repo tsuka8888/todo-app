d3ce66e92a04074af3c21c43b1dbd0a062e92df7
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2022-03-04 19:21:25 +0900

    TodoItem.tsxを追加

---
+++ src/molecules/TodoItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { IconButton, ListItem, ListItemText } from '@material-ui/core'
+import { TodoState } from '../modules/todos/types'
+import DeleteForever from '@material-ui/icons/DeleteForever'
+import Check from '@material-ui/icons/Check'
+import Restore from '@material-ui/icons/Restore'
+
+// TodoItemコンポーネントを作成
+export const TodoItem: React.VFC<TodoState> = (todo: TodoState) => {
+  return (
+    <ListItem>
+      <ListItemText>{todo.content}</ListItemText>
+      <IconButton>{todo.done ? <Restore /> : <Check />}</IconButton>
+      <IconButton>
+        <DeleteForever />
+      </IconButton>
+    </ListItem>
+  )
+}
