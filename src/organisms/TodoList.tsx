dafe79b4325fa81cf37a76e4ce9e5e4498dc2ad7
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2022-03-04 19:30:52 +0900

    TodoInput.tsx、TodoList.tsxを追加

---
+++ src/organisms/TodoList.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box, List, Paper, Typography } from '@material-ui/core'
+import { TodoState } from '../modules/todos/types'
+import { TodoItem } from '../molecules/TodoItem'
+
+interface TodoListProps {
+  todoList: TodoState[]
+  isIncompleteList?: boolean
+}
+
+// TodoListコンポーネントを作成
+// export const TodoList = ({todoList}:TodoListProps) => {
+export const TodoList: React.VFC<TodoListProps> = ({
+  todoList,
+  isIncompleteList = false,
+}: TodoListProps) => {
+  const title = isIncompleteList ? '未完了のTODO' : '完了のTODO'
+
+  return (
+    <Box mt={2}>
+      <Paper>
+        <Box p={4}>
+          <Typography variant="subtitle1">{title}</Typography>
+          <List>
+            {todoList.map((todo) => {
+              return <TodoItem key={todo.id} id={todo.id} content={todo.content} done={todo.done} />
+            })}
+          </List>
+        </Box>
+      </Paper>
+    </Box>
+  )
+}
